bb59868724774cea4c77499556a32db558c8fbf2
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-05-11 09:25:22 +0500

    condition hardcode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import { MdFingerprint } from 'react-icons/md';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { IconContext } from 'react-icons/lib';
-import fire from '../components/pages/fire'
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { MdFingerprint } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import fire from "../components/pages/fire";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -25,127 +25,160 @@ function Navbar() {
   const [user, setUser] = useState(() => {
     const user = fire.auth().currentUser;
 
-    return user;
-});
-
-useEffect(() => {
-  fire.auth().onAuthStateChanged(firebaseUser => {
-      setUser(firebaseUser);
+    return false;
   });
-}, [])
 
-
-  
+  useEffect(() => {
+    fire.auth().onAuthStateChanged((firebaseUser) => {
+      setUser(false);
+    });
+  }, []);
 
   useEffect(() => {
     showButton();
-    window.addEventListener('resize', showButton);
+    window.addEventListener("resize", showButton);
     return {
       // window.removeEventListener('resize', showButton)
-    }
+    };
   }, []);
 
-  
+  if (false) {
+    console.log("User: ", { user });
+    return(<Home />);
+  } else {
+    return(<LoginSignUp />);
+  }
 
+  function Home(props) {
+    return (
+      <>
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <nav className="navbar">
+            <div className="navbar-container container">
+              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                <MdFingerprint className="navbar-icon" />
+                Blockchain Land Records System
+              </Link>
+              <div className="menu-icon" onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+              </div>
+              <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                  <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                    Home
+                  </Link>
+                </li>
 
-  return (
-    <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <nav className='navbar'>
-          <div className='navbar-container container'>
-            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-              <MdFingerprint className='navbar-icon' />
-              Blockchain Land Records System
-            </Link>
-            <div className='menu-icon' onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
+                <li className="nav-item">
+                  <Link
+                    to="/services"
+                    className="nav-links"
+                    onClick={closeMobileMenu}
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/products"
+                    className="nav-links"
+                    onClick={closeMobileMenu}
+                  >
+                    Products
+                  </Link>
+                </li>
+
+                <li className="nav-btn">
+                  {button ? (
+                    <Link to="/" className="btn-link">
+                      <Button
+                        buttonStyle="btn--outline"
+                        onClick={() => fire.auth().signOut()}
+                      >
+                        SIGN OUT
+                      </Button>
+                    </Link>
+                  ) : (
+                    <Link to="/" className="btn-link">
+                      <Button
+                        buttonStyle="btn--outline"
+                        buttonSize="btn--mobile"
+                        onClick={() => fire.auth().signOut()}
+                      >
+                        SIGN OUT
+                      </Button>
+                    </Link>
+                  )}
+                </li>
+              </ul>
             </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-              <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                  Home
-                </Link>
-              </li>
-              
-              <li className='nav-item'>
-                <Link
-                  to='/services'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Services
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/products'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Products
-                </Link>
-              </li>
-              
-              
-              <li className='nav-btn'>
-                {button ? (
-                  <Link to='/' className='btn-link'>
-                    <Button buttonStyle='btn--outline' onClick={() => fire.auth().signOut()} >SIGN OUT</Button>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  }
+
+  function LoginSignUp(props) {
+    return (
+      <>
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <nav className="navbar">
+            <div className="navbar-container container">
+              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                <MdFingerprint className="navbar-icon" />
+                Blockchain Land Records System
+              </Link>
+              <div className="menu-icon" onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+              </div>
+              <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                  <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                    Home
                   </Link>
-                ) : (
-                  <Link to='/' className='btn-link'>
-                    <Button
-                      buttonStyle='btn--outline'
-                      buttonSize='btn--mobile'
-                      onClick={() => fire.auth().signOut()}
-                    >
-                      SIGN OUT
-                    </Button>
-                  </Link>
-                )}
-              </li>
-            
-              <li className='nav-btn'>
-                {button ? (
-                  <Link to='/sign-in' className='btn-link'>
-                    <Button buttonStyle='btn--outline'>SIGN IN</Button>
-                  </Link>
-                ) : (
-                  <Link to='/sign-in' className='btn-link'>
-                    <Button
-                      buttonStyle='btn--outline'
-                      buttonSize='btn--mobile'
-                      onClick={closeMobileMenu}
-                    >
-                      SIGN IN
-                    </Button>
-                  </Link>
-                )}
-              </li>
-              <li className='nav-btn'>
-                {button ? (
-                  <Link to='/sign-up' className='btn-link'>
-                    <Button buttonStyle='btn--outline'>SIGN UP</Button>
-                  </Link>
-                ) : (
-                  <Link to='/sign-up' className='btn-link'>
-                    <Button
-                      buttonStyle='btn--outline'
-                      buttonSize='btn--mobile'
-                      onClick={closeMobileMenu}
-                    >
-                      SIGN UP
-                    </Button>
-                  </Link>
-                )}
-              </li>
-        
-            </ul>
-          </div>
-        </nav>
-      </IconContext.Provider>
-    </>
-  );
+                </li>
+
+                <li className="nav-btn">
+                  {button ? (
+                    <Link to="/sign-in" className="btn-link">
+                      <Button buttonStyle="btn--outline">SIGN IN</Button>
+                    </Link>
+                  ) : (
+                    <Link to="/sign-in" className="btn-link">
+                      <Button
+                        buttonStyle="btn--outline"
+                        buttonSize="btn--mobile"
+                        onClick={closeMobileMenu}
+                      >
+                        SIGN IN
+                      </Button>
+                    </Link>
+                  )}
+                </li>
+                <li className="nav-btn">
+                  {button ? (
+                    <Link to="/sign-up" className="btn-link">
+                      <Button buttonStyle="btn--outline">SIGN UP</Button>
+                    </Link>
+                  ) : (
+                    <Link to="/sign-up" className="btn-link">
+                      <Button
+                        buttonStyle="btn--outline"
+                        buttonSize="btn--mobile"
+                        onClick={closeMobileMenu}
+                      >
+                        SIGN UP
+                      </Button>
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  }
 }
 
 export default Navbar;
