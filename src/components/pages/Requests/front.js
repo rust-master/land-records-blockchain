c090476d9adf1783e892d3594cc12b94821fe83f
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-05-24 12:50:20 +0500

    unsed imports removed

--- src/components/pages/Requests/front.js
+++ src/components/pages/Requests/front.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "./FrontSection.css";
-//import { Button } from "../../Button";
 import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
