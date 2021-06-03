5694daecd17a5fc62cb18bc7fb23e9c28b1c7928
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-03 12:30:25 +0500

    formatting

--- src/components/pages/SearchProperty/front.js
+++ src/components/pages/SearchProperty/front.js
@@ -4,7 +4,7 @@ import { Button } from "../../Button";
 import { Link } from "react-router-dom";
 
 class SearchProperty extends Component {
-  
+
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value,
