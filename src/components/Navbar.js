6dd3b5d3ca94a72f4f91b4da611f4f25fffef899
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-03 14:14:22 +0500

    new menu adding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,12 +105,18 @@ function Navbar() {
                   </Link>
                 </li>
 
-
                 <div className="menu-container">
                   <button onClick={onClick} className="menu-trigger">
                     <span>User</span>
                     <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/df/df7789f313571604c0e4fb82154f7ee93d9989c6.jpg" alt="User avatar" />
                   </button>
+                  <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+                    <ul>
+                      <li><a href="/messages">Messages</a></li>
+                      <li><a href="/trips">Trips</a></li>
+                      <li><a href="/saved">Saved</a></li>
+                    </ul>
+                  </nav>
                 </div>
 
                 {/* <li className="nav-btn">
