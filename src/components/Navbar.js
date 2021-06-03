f34144043e3ff8b11deab5b980e0c5f5285bed26
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-03 14:27:24 +0500

    menu added complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,29 @@ function Navbar() {
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
 
+ 
+
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      // If the active element exists and is clicked outside of
+      if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    };
+  
+    // If the item is active (ie open) then listen for clicks
+    if (isActive) {
+      window.addEventListener('click', pageClickEvent);
+    }
+  
+    return () => {
+      window.removeEventListener('click', pageClickEvent);
+    }
+  
+  }, [isActive]);
+
+  
+
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
