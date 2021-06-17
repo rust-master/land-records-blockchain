fa412272ea77e0ed12e670748aa971c8e9b958c4
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-17 09:26:59 +0500

    button changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-const COLOR = ['primary', 'blue', 'red', 'green' , 'pink'];
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
   children,
