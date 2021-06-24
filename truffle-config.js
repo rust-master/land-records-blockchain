8dfaf1e609bcc0161e49ed9ac1168873c0dd0292
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-24 13:21:32 +0500

    truffle changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,10 @@
+
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
     },
-    develop: {
-      port: 8545
-    }
-  }
+  },
 };
