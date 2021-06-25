e4b3bf111a292bdecd7ccdc53a63abb63cd84195
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-25 23:02:49 +0500

    formatting

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,70 +1,70 @@
-import React from 'react';
-import { Button } from './Button';
-import './Property.css';
-import { FaFire } from 'react-icons/fa';
-import { BsXDiamondFill } from 'react-icons/bs';
-import { GiCrystalize } from 'react-icons/gi';
-import { IconContext } from 'react-icons/lib';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button } from "./Button";
+import "./Property.css";
+import { FaFire } from "react-icons/fa";
+import { BsXDiamondFill } from "react-icons/bs";
+import { GiCrystalize } from "react-icons/gi";
+import { IconContext } from "react-icons/lib";
+import { Link } from "react-router-dom";
 
 function Property() {
   return (
-    <IconContext.Provider value={{ color: '#fff', size: 64 }}>
-      <div className='pricing__section'>
-        <div className='pricing__wrapper'>
-          <h1 className='pricing__heading'>Properties</h1>
-          <div className='pricing__container'>
-            <Link to='/sign-up' className='pricing__container-card'>
-              <div className='pricing__container-cardInfo'>
-                <div className='icon'>
+    <IconContext.Provider value={{ color: "#fff", size: 64 }}>
+      <div className="pricing__section">
+        <div className="pricing__wrapper">
+          <h1 className="pricing__heading">Properties</h1>
+          <div className="pricing__container">
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
                   <BsXDiamondFill />
                 </div>
                 <h3>Land</h3>
                 <h4>10 Ether</h4>
                 <p>value</p>
-                <ul className='pricing__container-features'>
+                <ul className="pricing__container-features">
                   <li>Property No: 1234</li>
                   <li>City: Sahiwal</li>
                   <li>Measurements: 120 sq/ft</li>
                 </ul>
-                <Button buttonSize='btn--wide' buttonColor='primary'>
+                <Button buttonSize="btn--wide" buttonColor="primary">
                   Mark Available
                 </Button>
               </div>
             </Link>
-            <Link to='/sign-up' className='pricing__container-card'>
-              <div className='pricing__container-cardInfo'>
-                <div className='icon'>
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
                   <BsXDiamondFill />
                 </div>
                 <h3>Land</h3>
                 <h4>20 Ether</h4>
                 <p>value</p>
-                <ul className='pricing__container-features'>
+                <ul className="pricing__container-features">
                   <li>Property No: 1234</li>
                   <li>City: Sahiwal</li>
                   <li>Measurements: 120 sq/ft</li>
                 </ul>
-                <Button buttonSize='btn--wide' buttonColor='blue'>
-                Mark Available
+                <Button buttonSize="btn--wide" buttonColor="blue">
+                  Mark Available
                 </Button>
               </div>
             </Link>
-            <Link to='/sign-up' className='pricing__container-card'>
-              <div className='pricing__container-cardInfo'>
-                <div className='icon'>
+            <Link to="/sign-up" className="pricing__container-card">
+              <div className="pricing__container-cardInfo">
+                <div className="icon">
                   <BsXDiamondFill />
                 </div>
                 <h3>Land</h3>
                 <h4>20 Ether</h4>
                 <p>value</p>
-                <ul className='pricing__container-features'>
+                <ul className="pricing__container-features">
                   <li>Property No: 1234</li>
                   <li>City: Sahiwal</li>
                   <li>Measurements: 120 sq/ft</li>
                 </ul>
-                <Button buttonSize='btn--wide' buttonColor='primary'>
-                Mark Available
+                <Button buttonSize="btn--wide" buttonColor="primary">
+                  Mark Available
                 </Button>
               </div>
             </Link>
