7a887194f8f77244c1ab324d09f28a6d3fee26fa
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-26 22:59:07 +0500

    fomratting

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -1,32 +1,42 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Land {    
+contract Land {
+    struct PropertyDetail {
+        uint256 value;
+        address currOwner;
+    }
 
-	struct PropertyDetail {
-		uint value;
-		address currOwner;
-	}
-	
-    mapping(uint => PropertyDetail) public properties;
-    uint[] public propertiesValues;
+    mapping(uint256 => PropertyDetail) public properties;
+    uint256[] public propertiesValues;
     address[] public propertiesAddress;
-    
-	// Create Property
-    function createProperty(uint _propId, uint _value, address _owner) public returns (bool)  {
-		properties[_propId] = PropertyDetail(_value, _owner);
-		propertiesValues.push(_value);
-		propertiesAddress.push(_owner);
-		return true;
-	}
-	
-	// Get the property details.
-	 function getPropertyDetails(uint _propId) view public returns (uint, address)  {
-		return (properties[_propId].value, properties[_propId].currOwner);
-	}
-	
-    // Get the property details.
-	 function getAllDetails() view public returns (uint[] memory, address[] memory)  {
-		return (propertiesValues,propertiesAddress);
-	}
 
+    // Create Property
+    function createProperty(
+        uint256 _propId,
+        uint256 _value,
+        address _owner
+    ) public returns (bool) {
+        properties[_propId] = PropertyDetail(_value, _owner);
+        propertiesValues.push(_value);
+        propertiesAddress.push(_owner);
+        return true;
+    }
+
+    // Get the property details.
+    function getPropertyDetails(uint256 _propId)
+        public
+        view
+        returns (uint256, address)
+    {
+        return (properties[_propId].value, properties[_propId].currOwner);
+    }
+
+    // Get the property details.
+    function getAllDetails()
+        public
+        view
+        returns (uint256[] memory, address[] memory)
+    {
+        return (propertiesValues, propertiesAddress);
+    }
 }
