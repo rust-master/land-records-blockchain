5126a8563b844c6eae146920e423291af553a8c9
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-26 11:42:20 +0500

    Smart Contract Changes in Land

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -8,16 +8,25 @@ contract Land {
 	}
 	
     mapping(uint => PropertyDetail) public properties;
-
+    uint[] public propertiesValues;
+    address[] public propertiesAddress;
+    
 	// Create Property
     function createProperty(uint _propId, uint _value, address _owner) public returns (bool)  {
 		properties[_propId] = PropertyDetail(_value, _owner);
+		propertiesValues.push(_value);
+		propertiesAddress.push(_owner);
 		return true;
 	}
 	
 	// Get the property details.
 	 function getPropertyDetails(uint _propId) view public returns (uint, address)  {
 		return (properties[_propId].value, properties[_propId].currOwner);
+	}
+	
+    // Get the property details.
+	 function getAllDetails() view public returns (uint[] memory, address[] memory)  {
+		return (propertiesValues,propertiesAddress);
 	}
 
 }
