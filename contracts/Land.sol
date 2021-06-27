0e496c80c98773453c1c105083a45593ee6890fc
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-27 12:46:15 +0500

    land contract changes

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -1,42 +1,35 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Land {
-    struct PropertyDetail {
-        uint256 value;
-        address currOwner;
-    }
+contract Land {    
 
-    mapping(uint256 => PropertyDetail) public properties;
-    uint256[] public propertiesValues;
+	struct PropertyDetail {
+	    uint landID;
+		uint value;
+		address currOwner;
+	}
+	
+    mapping(uint => PropertyDetail) public properties;
+    uint[] public propertiesIDs;
+    uint[] public propertiesValues;
     address[] public propertiesAddress;
-
-    // Create Property
-    function createProperty(
-        uint256 _propId,
-        uint256 _value,
-        address _owner
-    ) public returns (bool) {
-        properties[_propId] = PropertyDetail(_value, _owner);
-        propertiesValues.push(_value);
-        propertiesAddress.push(_owner);
-        return true;
-    }
-
+    
+	// Create Property
+    function createProperty(uint _propId, uint _value, address _owner) public returns (bool)  {
+		properties[_propId] = PropertyDetail(_propId, _value, _owner);
+		propertiesIDs.push(_propId);
+		propertiesValues.push(_value);
+		propertiesAddress.push(_owner);
+		return true;
+	}
+	
+	// Get the property details.
+	 function getPropertyDetails(uint _propId) view public returns (uint, uint, address)  {
+		return (properties[_propId].landID, properties[_propId].value, properties[_propId].currOwner);
+	}
+	
     // Get the property details.
-    function getPropertyDetails(uint256 _propId)
-        public
-        view
-        returns (uint256, address)
-    {
-        return (properties[_propId].value, properties[_propId].currOwner);
-    }
+	 function getAllDetails() view public returns (uint[] memory, uint[] memory, address[] memory)  {
+		return (propertiesIDs, propertiesValues,propertiesAddress);
+	}
 
-    // Get the property details.
-    function getAllDetails()
-        public
-        view
-        returns (uint256[] memory, address[] memory)
-    {
-        return (propertiesValues, propertiesAddress);
-    }
 }
