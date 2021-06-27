df4c9a3ea6667c06df77392bbde292552ff09283
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-27 12:07:08 +0500

    romoved unused code

--- src/components/pages/SearchProperty/front.js
+++ src/components/pages/SearchProperty/front.js
@@ -8,7 +8,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import logo from "../SearchProperty/decreasing.png";
 import Typography from "@material-ui/core/Typography";
 
 import Web3 from "web3";
