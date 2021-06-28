53648108a2c6e4f31aad3b1a67bbe9931063944b
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-28 23:09:55 +0500

    land contract update

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -6,19 +6,25 @@ contract Land {
 	    uint landID;
 		uint value;
 		address currOwner;
+		string city;
+		string measurement;
 	}
 	
     mapping(uint => PropertyDetail) public properties;
     uint[] public propertiesIDs;
     uint[] public propertiesValues;
     address[] public propertiesAddress;
+    string[] public propertiesCity;
+    string[] public propertiesMeasurement;
     
 	// Create Property
-    function createProperty(uint _propId, uint _value, address _owner) public returns (bool)  {
-		properties[_propId] = PropertyDetail(_propId, _value, _owner);
+    function createProperty(uint _propId, uint _value, address _owner, string memory _city, string memory _measurement) public returns (bool)  {
+		properties[_propId] = PropertyDetail(_propId, _value, _owner, _city, _measurement);
 		propertiesIDs.push(_propId);
 		propertiesValues.push(_value);
 		propertiesAddress.push(_owner);
+		propertiesCity.push(_city);
+		propertiesMeasurement.push(_measurement);
 		return true;
 	}
 	
@@ -28,8 +34,8 @@ contract Land {
 	}
 	
     // Get the property details.
-	 function getAllDetails() view public returns (uint[] memory, uint[] memory, address[] memory)  {
-		return (propertiesIDs, propertiesValues,propertiesAddress);
+	 function getAllDetails() view public returns (uint[] memory, uint[] memory, address[] memory, string[] memory, string[] memory)  {
+		return (propertiesIDs, propertiesValues,propertiesAddress, propertiesCity,propertiesMeasurement);
 	}
 
 }
