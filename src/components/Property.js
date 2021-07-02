4b8f153c70a463a68cd428976615d67b31e20afb
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-07-02 18:56:08 +0500

    changes in propert.js

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,4 +1,4 @@
-import React, {  } from "react";
+import React, { Component } from "react";
 import { Button } from "./Button";
 import "./Property.css";
 import { FaFire } from "react-icons/fa";
@@ -14,17 +14,27 @@ import contract from "../build/contracts/Land.json";
 
 
 
-function Property() {
+class Property extends Component {
 
-  const [ids, setids] = useState([]);
-  const [owner, setOwner] = useState([]);
-  const [landValue, setlandValue] = useState([]);
-  const [city, setCity] = useState([]);
-  const [measure, setMeasure] = useState([]);
-  const [status, setStatus] = useState([]);
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
 
-  const loadBlockchainData = async () => {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsIds: [],
+      itemsOwner: [],
+      itemsValues: [],
+      itemsCity: [],
+      itemsMeasure: [],
+      itemsStatus: [],
+    };
+  }
+
+
+  async loadBlockchainData() {
+
     const web3 = window.web3;
     const landCon = new web3.eth.Contract(contract.abi, "0x6E68253Bb0286f977B27704b82bCC511252D4845");
 
@@ -32,82 +42,93 @@ function Property() {
 
 
     for (const [index, value] of allLands["0"].entries()) {
-      console.log({ value })
+      this.state.itemsIds.push(value);
     }
 
     for (const [index, value] of allLands["1"].entries()) {
-      console.log({ value })
+      this.state.itemsValues.push(value);
     }
 
     for (const [index, value] of allLands["2"].entries()) {
-      owner.push("12")
-      console.log({ value })
-      //setOwner(owner)
+      this.state.itemsOwner.push(value);
     }
 
-    
-
     for (const [index, value] of allLands["3"].entries()) {
-      console.log({ value })
+      this.state.itemsCity.push(value);
     }
 
     for (const [index, value] of allLands["4"].entries()) {
-      console.log({ value })
+      this.state.itemsMeasure.push(value);
     }
 
     for (const [index, value] of allLands["5"].entries()) {
-      console.log({ value })
+      this.state.itemsStatus.push(value);
     }
 
   };
 
 
-  useEffect(() => {
 
-    loadBlockchainData()
+  render() {
 
-    console.log("Type: " + owner)
+    const ids = this.state.itemsIds;
+    const address = this.state.itemsOwner;
+    const price = this.state.itemsValues;
+    const city = this.state.itemsCity;
+    const measure = this.state.itemsMeasure;
+    const status = this.state.itemsStatus;
 
-    const result2 = owner.filter(letter => letter.length > 4);
+    const result2 = address.filter(letter => letter.length > 4);
 
     console.log(result2);
-  
-
-  }, []);
 
 
- 
-  return (
-    <IconContext.Provider value={{ color: "#fff", size: 64 }}>
-      <div className="pricing__section">
-        <div className="pricing__wrapper">
-          <h1 className="pricing__heading">Properties</h1>
-          <div className="pricing__container">
+    let ListTemplate;
+
+    if (status.length) {
+      ListTemplate = address.filter(item => item != "Not-Available-For-Sale").map((value, index) =>
+
+        <Link to="/sign-up" className="pricing__container-card">
+          <div className="pricing__container-cardInfo">
+            <div className="icon">
+              <BsXDiamondFill />
+            </div>
+            <h3>Land</h3>
+            <h4>{price[index]} Ether</h4>
+            <p>value</p>
+            <ul className="pricing__container-features">
+              <li>Property No: {ids[index]}</li>
+              <li>City: {city[index]}</li>
+              <li>Measurements: {measure[index]} sq/ft</li>
+            </ul>
+            <Button buttonSize="btn--wide" buttonColor="primary">
+              Mark Available
+            </Button>
+          </div>
+        </Link>
+      );
+    }
+    else {
+      ListTemplate = <div > Records Not Found </div>;
+    }
 
 
-            <Link to="/sign-up" className="pricing__container-card">
-              <div className="pricing__container-cardInfo">
-                <div className="icon">
-                  <BsXDiamondFill />
-                </div>
-                <h3>Land</h3>
-                <h4>10 Ether</h4>
-                <p>value</p>
-                <ul className="pricing__container-features">
-                  <li>Property No: 1234</li>
-                  <li>City: Sahiwal</li>
-                  <li>Measurements: 120 sq/ft</li>
-                </ul>
-                <Button buttonSize="btn--wide" buttonColor="primary">
-                  Mark Available
-                </Button>
-              </div>
-            </Link>
 
+    return (
+      <IconContext.Provider value={{ color: "#fff", size: 64 }}>
+        <div className="pricing__section">
+          <div className="pricing__wrapper">
+            <h1 className="pricing__heading">Properties</h1>
+            <div className="pricing__container">
+
+              {ListTemplate}
+
+
+            </div>
           </div>
         </div>
-      </div>
-    </IconContext.Provider>
-  );
+      </IconContext.Provider>
+    );
+  }
 }
 export default Property;
