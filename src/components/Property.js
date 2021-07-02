e00880bb6098984baeb372f08ad6ab6943377437
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-07-02 18:42:27 +0500

    changes

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, {  } from "react";
 import { Button } from "./Button";
 import "./Property.css";
 import { FaFire } from "react-icons/fa";
@@ -7,6 +7,8 @@ import { GiCrystalize } from "react-icons/gi";
 import { IconContext } from "react-icons/lib";
 import { Link } from "react-router-dom";
 
+import xtype from 'xtypejs'
+
 import contract from "../build/contracts/Land.json";
 
 
@@ -14,47 +16,67 @@ import contract from "../build/contracts/Land.json";
 
 function Property() {
 
+  const [ids, setids] = useState([]);
+  const [owner, setOwner] = useState([]);
+  const [landValue, setlandValue] = useState([]);
+  const [city, setCity] = useState([]);
+  const [measure, setMeasure] = useState([]);
+  const [status, setStatus] = useState([]);
 
   const loadBlockchainData = async () => {
+    
     const web3 = window.web3;
-    const landCon = new web3.eth.Contract(contract.abi,"0x6E68253Bb0286f977B27704b82bCC511252D4845");
+    const landCon = new web3.eth.Contract(contract.abi, "0x6E68253Bb0286f977B27704b82bCC511252D4845");
 
     const allLands = await landCon.methods.getAllDetails().call();
 
 
     for (const [index, value] of allLands["0"].entries()) {
-      console.log({value})    
+      console.log({ value })
     }
 
     for (const [index, value] of allLands["1"].entries()) {
-      console.log({value})    
+      console.log({ value })
     }
 
     for (const [index, value] of allLands["2"].entries()) {
-      console.log({value})    
+      owner.push("12")
+      console.log({ value })
+      //setOwner(owner)
     }
 
+    
+
     for (const [index, value] of allLands["3"].entries()) {
-      console.log({value})    
+      console.log({ value })
     }
 
     for (const [index, value] of allLands["4"].entries()) {
-      console.log({value})    
+      console.log({ value })
     }
 
     for (const [index, value] of allLands["5"].entries()) {
-      console.log({value})    
+      console.log({ value })
     }
 
-   };
+  };
 
 
   useEffect(() => {
 
     loadBlockchainData()
 
+    console.log("Type: " + owner)
+
+    const result2 = owner.filter(letter => letter.length > 4);
+
+    console.log(result2);
+  
+
   }, []);
 
+
+ 
   return (
     <IconContext.Provider value={{ color: "#fff", size: 64 }}>
       <div className="pricing__section">
