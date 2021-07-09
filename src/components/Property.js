80f72ad385d7489f7e06bd0f3f099d46e40f066e
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-07-09 16:02:12 +0500

    changes

--- src/components/Property.js
+++ src/components/Property.js
@@ -52,7 +52,7 @@ class Property extends Component {
 
       const web3 = window.web3
 
-      const landCon = new web3.eth.Contract(contract.abi, "0x6fDdC7751485d56B95Cd6d2904C1aef3b9ee1104")
+      const landCon = new web3.eth.Contract(contract.abi, "0x874026dD1Bd778AAE6fa4eF95D5004091E5505b9")
 
       await landCon.methods.makeAvailable(id).send({ from: this.state.account })
 
@@ -75,7 +75,7 @@ class Property extends Component {
     this.setState({ account: accounts[0] })
     console.log("Account: " + this.state.account);
 
-    const landCon = new web3.eth.Contract(contract.abi, "0x6fDdC7751485d56B95Cd6d2904C1aef3b9ee1104")
+    const landCon = new web3.eth.Contract(contract.abi, "0x874026dD1Bd778AAE6fa4eF95D5004091E5505b9")
 
     const assets = await landCon.methods.viewAssets().call({ from: this.state.account })
 
