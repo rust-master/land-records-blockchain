23e24b85eac12ec2e16c4fa5cccbe86218bff75f
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-07-15 17:19:57 +0500

    front data changes

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -3,7 +3,7 @@ export const homeObjOne = {
   lightText: true,
   lightTextDesc: true,
   topLine: 'Exclusive Access',
-  headline: 'Unlimited Transactions with zero fees',
+  headline: 'Unlimited Transactions with gas fees',
   description:
     'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
   buttonLabel: 'Get Started',
