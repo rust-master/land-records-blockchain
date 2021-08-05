fe7f413734308ea3e05f254ef4be6823b7621d2f
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-05 19:40:46 +0500

    formatting

--- src/components/Property.js
+++ src/components/Property.js
@@ -4,27 +4,24 @@ import "./Property.css";
 import { IconContext } from "react-icons/lib";
 import { Link } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 
 import Web3 from "web3";
 import Slide from "@material-ui/core/Slide";
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 import contract from "../build/contracts/Land.json";
-
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-
 class Property extends Component {
-
   componentWillMount() {
     this.loadBlockchainData();
   }
@@ -49,86 +46,96 @@ class Property extends Component {
     };
   }
 
-
   async myFunction(id, status) {
     if (!status) {
-      console.log("ID : " + id)
-      console.log("Account: " + this.state.account)
+      console.log("ID : " + id);
+      console.log("Account: " + this.state.account);
 
-      const web3 = window.web3
+      const web3 = window.web3;
 
-      const landCon = new web3.eth.Contract(contract.abi, "0x42D108627ebA89Eb1006224c26a03BF2DaC24f8f")
+      const landCon = new web3.eth.Contract(
+        contract.abi,
+        "0x42D108627ebA89Eb1006224c26a03BF2DaC24f8f"
+      );
 
-      await landCon.methods.makeAvailable(id).send({ from: this.state.account })
+      await landCon.methods
+        .makeAvailable(id)
+        .send({ from: this.state.account });
 
-      this.setState({ open: true })
+      this.setState({ open: true });
 
       this.loadBlockchainData();
-
     } else {
-      console.log("Status : " + status)
-      this.setState({ openi: true })
+      console.log("Status : " + status);
+      this.setState({ openi: true });
     }
   }
 
-
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
 
-    const webeProvider = new Web3(Web3.givenProvider || "http://localhost:7545")
-    const accounts = await webeProvider.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const webeProvider = new Web3(
+      Web3.givenProvider || "http://localhost:7545"
+    );
+    const accounts = await webeProvider.eth.getAccounts();
+    this.setState({ account: accounts[0] });
     console.log("Account: " + this.state.account);
 
-    const landCon = new web3.eth.Contract(contract.abi, "0x42D108627ebA89Eb1006224c26a03BF2DaC24f8f")
+    const landCon = new web3.eth.Contract(
+      contract.abi,
+      "0x42D108627ebA89Eb1006224c26a03BF2DaC24f8f"
+    );
 
-    const assets = await landCon.methods.viewAssets().call({ from: this.state.account })
+    const assets = await landCon.methods
+      .viewAssets()
+      .call({ from: this.state.account });
 
-    this.state.allAssets = assets
+    this.state.allAssets = assets;
     for (let i = 0; i <= assets.length; i++) {
-      this.state.ids.push(assets[i])
+      this.state.ids.push(assets[i]);
     }
 
-
     this.state.allAssets.map(async (value, index) => {
-      const detail = await landCon.methods.landInfoOwner(this.state.ids[index]).call({ from: this.state.account })
-      const remainignDetail = await landCon.methods.remainingDetail(this.state.ids[index]).call({ from: this.state.account })
+      const detail = await landCon.methods
+        .landInfoOwner(this.state.ids[index])
+        .call({ from: this.state.account });
+      const remainignDetail = await landCon.methods
+        .remainingDetail(this.state.ids[index])
+        .call({ from: this.state.account });
 
-      this.state.states.push(detail[0])
-      this.state.district.push(detail[1])
-      this.state.village.push(detail[2])
-      this.state.survyNo.push(detail[3])
-      this.state.status.push(detail[4])
-      this.state.marketValue.push(detail[5])
-      this.state.measurements.push(detail[6])
+      this.state.states.push(detail[0]);
+      this.state.district.push(detail[1]);
+      this.state.village.push(detail[2]);
+      this.state.survyNo.push(detail[3]);
+      this.state.status.push(detail[4]);
+      this.state.marketValue.push(detail[5]);
+      this.state.measurements.push(detail[6]);
 
-      this.state.ipfsHash.push(remainignDetail)
+      this.state.ipfsHash.push(remainignDetail);
 
-      console.log("State: " + detail[0])
-      console.log("Destrict: " + detail[1])
-      console.log("Village: " + detail[2])
-      console.log("Status: " + detail[4])
-      console.log("marketValue: " + detail[5])
-      console.log("measurements: " + detail[6])
+      console.log("State: " + detail[0]);
+      console.log("Destrict: " + detail[1]);
+      console.log("Village: " + detail[2]);
+      console.log("Status: " + detail[4]);
+      console.log("marketValue: " + detail[5]);
+      console.log("measurements: " + detail[6]);
 
-      console.log("ipfsHash: " + remainignDetail)
+      console.log("ipfsHash: " + remainignDetail);
 
-      console.log("---------------------------------")
-    })
+      console.log("---------------------------------");
+    });
   }
 
   handleClose(e, r) {
-    if (r === 'clickaway') {
+    if (r === "clickaway") {
       return;
     }
 
-    this.setState({ open: false })
-    this.setState({ openi: false })
+    this.setState({ open: false });
+    this.setState({ openi: false });
   }
 
-
   render() {
-
     const dataAll = this.state.allAssets;
     const statesAll = this.state.states;
     const districtAll = this.state.district;
@@ -141,18 +148,29 @@ class Property extends Component {
 
     const ipfsAll = this.state.ipfsHash;
 
-    let ListTemplate
+    let ListTemplate;
 
     if (dataAll.length) {
-
-      ListTemplate = dataAll.map((value, index) =>
-
-        <Slide direction="left" in={true} timeout={1500} mountOnEnter unmountOnExit>
-
-          <div style={{ maxWidth: "1030px", marginBottom: "60px", marginLeft: "5.5%" }}>
-            <Card className={makeStyles({
-              maxWidth: 345,
-            })}>
+      ListTemplate = dataAll.map((value, index) => (
+        <Slide
+          direction="left"
+          in={true}
+          timeout={1500}
+          mountOnEnter
+          unmountOnExit
+        >
+          <div
+            style={{
+              maxWidth: "1030px",
+              marginBottom: "60px",
+              marginLeft: "5.5%",
+            }}
+          >
+            <Card
+              className={makeStyles({
+                maxWidth: 345,
+              })}
+            >
               <CardMedia
                 component="img"
                 alt="Image not load"
@@ -182,20 +200,24 @@ class Property extends Component {
                 </Typography>
               </CardContent>
               <CardActions style={{ float: "right" }}>
-                <Button buttonSize="btn--wide" buttonColor={statusAll[index] ? "red" : "blue"}
+                <Button
+                  buttonSize="btn--wide"
+                  buttonColor={statusAll[index] ? "red" : "blue"}
                   disabled={statusAll[index]}
-                  onClick={this.myFunction.bind(this, idsAll[index], statusAll[index])} >
+                  onClick={this.myFunction.bind(
+                    this,
+                    idsAll[index],
+                    statusAll[index]
+                  )}
+                >
                   {statusAll[index] ? "Marked" : "Mark Available"}
                 </Button>
               </CardActions>
             </Card>
           </div>
-
         </Slide>
-
-      );
-    }
-    else {
+      ));
+    } else {
       ListTemplate = <div>Records Not Found</div>;
     }
 
@@ -209,18 +231,25 @@ class Property extends Component {
 
             {ListTemplate}
 
-            <Snackbar open={this.state.open} autoHideDuration={6000} onClose={this.handleClose}>
+            <Snackbar
+              open={this.state.open}
+              autoHideDuration={6000}
+              onClose={this.handleClose}
+            >
               <Alert onClose={this.handleClose} severity="success">
                 Land Marked successfully.
               </Alert>
             </Snackbar>
 
-            <Snackbar open={this.state.openi} autoHideDuration={6000} onClose={this.handleClose}>
+            <Snackbar
+              open={this.state.openi}
+              autoHideDuration={6000}
+              onClose={this.handleClose}
+            >
               <Alert onClose={this.handleClose} severity="error">
                 {"Already Marked"}
               </Alert>
             </Snackbar>
-
           </div>
         </div>
       </div>
