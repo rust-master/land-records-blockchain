b10a50b12dd5bb247bd4046788d27185c01af965
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-06 14:49:17 +0500

    formatting

--- src/components/pages/Requests/front.js
+++ src/components/pages/Requests/front.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import "./FrontSection.css";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 import Web3 from "web3";
-import contract from "../../../build/contracts/Land.json"
+import contract from "../../../build/contracts/Land.json";
 
 class RequestsFront extends Component {
-
   componentWillMount() {
     this.loadBlockchainData();
   }
-
 
   constructor(props) {
     super(props);
@@ -30,7 +28,7 @@ class RequestsFront extends Component {
       requester: [],
       requestStatus: [],
       account: "",
-    }
+    };
   }
 
   async loadBlockchainData() {
@@ -40,7 +38,7 @@ class RequestsFront extends Component {
       Web3.givenProvider || "http://localhost:7545"
     );
     const accounts = await webeProvider.eth.getAccounts();
-    
+
     this.setState({ account: accounts[0] });
     console.log("Account: " + this.state.account);
 
@@ -50,8 +48,8 @@ class RequestsFront extends Component {
     );
 
     const assets = await landCon.methods
-    .viewAssets()
-    .call({ from: this.state.account });
+      .viewAssets()
+      .call({ from: this.state.account });
 
     this.state.allAssets = assets;
     for (let i = 0; i <= assets.length; i++) {
@@ -59,11 +57,9 @@ class RequestsFront extends Component {
     }
 
     this.state.allAssets.map(async (value, index) => {
-      
       const detail = await landCon.methods
         .landInfoUser(this.state.ids[index])
         .call({ from: this.state.account });
-        
 
       this.state.owner.push(detail[0]);
       this.state.marketValue.push(detail[1]);
@@ -71,20 +67,15 @@ class RequestsFront extends Component {
       this.state.requester.push(detail[3]);
       this.state.requestStatus.push(detail[4]);
 
-
-
       console.log("owner: " + detail[0]);
       console.log("marketValue: " + detail[1]);
       console.log("status: " + detail[2]);
       console.log("requester: " + detail[3]);
       console.log("requestStatus: " + detail[4]);
 
-
       console.log("---------------------------------");
     });
-
   }
-
 
   render() {
     return (
@@ -108,30 +99,56 @@ class RequestsFront extends Component {
                   </h1>
                   <div className="input-areas">
                     <div>
-                      <Card className={makeStyles({
-                        maxWidth: 345,
-                      })}>
+                      <Card
+                        className={makeStyles({
+                          maxWidth: 345,
+                        })}
+                      >
                         <CardActionArea>
                           <CardContent>
-                            <Typography gutterBottom variant="h4" component="h2">
+                            <Typography
+                              gutterBottom
+                              variant="h4"
+                              component="h2"
+                            >
                               Property No: 1234
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Value: 20 Ether
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Location: Sahiwal
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Measurements: 120 sq/ft
                             </Typography>
                           </CardContent>
                         </CardActionArea>
                         <CardActions>
-                          <Button size="small" variant="contained" color="secondary">
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="secondary"
+                          >
                             Reject
                           </Button>
-                          <Button size="small" variant="contained" color="primary" >
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                          >
                             Accept
                           </Button>
                         </CardActions>
@@ -139,30 +156,56 @@ class RequestsFront extends Component {
                     </div>
 
                     <div style={{ marginTop: 40 }}>
-                      <Card className={makeStyles({
-                        maxWidth: 345,
-                      })}>
+                      <Card
+                        className={makeStyles({
+                          maxWidth: 345,
+                        })}
+                      >
                         <CardActionArea>
                           <CardContent>
-                            <Typography gutterBottom variant="h4" component="h2">
+                            <Typography
+                              gutterBottom
+                              variant="h4"
+                              component="h2"
+                            >
                               Property No: 2334
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Value: 40 Ether
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Location: Lahore
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Measurements: 110 sq/ft
                             </Typography>
                           </CardContent>
                         </CardActionArea>
                         <CardActions>
-                          <Button size="small" variant="contained" color="secondary">
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="secondary"
+                          >
                             Reject
                           </Button>
-                          <Button size="small" variant="contained" color="primary" >
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                          >
                             Accept
                           </Button>
                         </CardActions>
@@ -170,30 +213,56 @@ class RequestsFront extends Component {
                     </div>
 
                     <div style={{ marginTop: 40 }}>
-                      <Card className={makeStyles({
-                        maxWidth: 345,
-                      })}>
+                      <Card
+                        className={makeStyles({
+                          maxWidth: 345,
+                        })}
+                      >
                         <CardActionArea>
                           <CardContent>
-                            <Typography gutterBottom variant="h4" component="h2">
+                            <Typography
+                              gutterBottom
+                              variant="h4"
+                              component="h2"
+                            >
                               Property No: 1454
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Value: 20 Ether
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Location: Sahiwal
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Measurements: 120 sq/ft
                             </Typography>
                           </CardContent>
                         </CardActionArea>
                         <CardActions>
-                          <Button size="small" variant="contained" color="secondary">
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="secondary"
+                          >
                             Reject
                           </Button>
-                          <Button size="small" variant="contained" color="primary" >
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                          >
                             Accept
                           </Button>
                         </CardActions>
@@ -201,30 +270,56 @@ class RequestsFront extends Component {
                     </div>
 
                     <div style={{ marginTop: 40 }}>
-                      <Card className={makeStyles({
-                        maxWidth: 345,
-                      })}>
+                      <Card
+                        className={makeStyles({
+                          maxWidth: 345,
+                        })}
+                      >
                         <CardActionArea>
                           <CardContent>
-                            <Typography gutterBottom variant="h4" component="h2">
+                            <Typography
+                              gutterBottom
+                              variant="h4"
+                              component="h2"
+                            >
                               Property No: 2554
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Value: 30 Ether
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Location: Karachi
                             </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                            >
                               Measurements: 130 sq/ft
                             </Typography>
                           </CardContent>
                         </CardActionArea>
                         <CardActions>
-                          <Button size="small" variant="contained" color="secondary">
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="secondary"
+                          >
                             Reject
                           </Button>
-                          <Button size="small" variant="contained" color="primary" >
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                          >
                             Accept
                           </Button>
                         </CardActions>
