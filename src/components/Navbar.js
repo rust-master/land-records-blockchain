fa9b67d5ee1a6cfbe177a93a1f4a2ce4179d7c4e
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-08-28 13:30:41 +0500

    nav changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -12,86 +12,96 @@ import "../App.css";
 import Web3 from "web3";
 import Cookies from "universal-cookie";
 
-function Navbar() {
-  const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
-  const onClick = () => setIsActive(!isActive);
+const cookies = new Cookies();
 
-  const [name, setName] = useState(false);
-  const [profileLink, setprofileLink] = useState(false);
-  const [balance, setbalance] = useState("");
+class Navbar extends React.Component {
 
-  const cookies = new Cookies();
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.showButton = this.showButton.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.closeMobileMenu = this.closeMobileMenu.bind(this);
+    this.state = {
+      isActive: false,
+      name: false,
+      balance: "",
+      dropdownRef: null,
+      click: false,
+      button: true,
+      cookieUser: cookies.get("checkIsUser"),
+    };
+  }
 
-  useEffect(() => {
-    const pageClickEvent = (e) => {
-      // If the active element exists and is clicked outside of
-      if (
-        dropdownRef.current !== null &&
-        !dropdownRef.current.contains(e.target)
-      ) {
-        setIsActive(!isActive);
+
+  componentWillMount() {
+    console.log("isActive", this.state.isActive);
+
+    if (this.state.isActive) {
+      this.pageClickEvent();
+
+      if (this.state.isActive) {
+        window.addEventListener("click", this.state.pageClickEvent);
       }
-    };
 
-    // If the item is active (ie open) then listen for clicks
-    if (isActive) {
-      window.addEventListener("click", pageClickEvent);
+      window.removeEventListener("click", this.state.pageClickEvent);
     }
 
-    return () => {
-      window.removeEventListener("click", pageClickEvent);
-    };
-  }, [isActive]);
+    this.showButton();
 
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+    window.addEventListener("resize", this.showButton());
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+    this.setState({ name: cookies.get("Username") });
+    console.log("name", this.state.name);
 
-  const showButton = () => {
+    this.getBalance();
+  }
+
+
+  onClick() {
+    this.setState({ isActive: !this.state.isActive });
+  }
+
+  pageClickEvent(e) {
+    if (
+      this.state.dropdownRef.current !== null &&
+      !this.state.dropdownRef.current.contains(e.target)
+    ) {
+      this.setState({ isActive: !this.state.isActive });
+    }
+  }
+
+  handleClick() {
+    this.setState({ click: !this.state.click });
+  }
+
+  closeMobileMenu() {
+    this.setState({ click: false });
+  }
+
+  showButton() {
     if (window.innerWidth <= 960) {
-      setButton(false);
+      this.setState({ button: false });
     } else {
-      setButton(true);
+      this.setState({ button: true });
     }
-  };
+  }
 
+  async getBalance() {
+    const web3 = window.web3;
+    const webeProvider = new Web3(
+      Web3.givenProvider || "http://localhost:7545"
+    );
+    const accounts = await webeProvider.eth.getAccounts();
 
+    const blnce = web3.utils.fromWei(
+      await web3.eth.getBalance(accounts[0]),
+      "ether"
+    );
+    console.log(blnce);
+    this.setState({ balance: blnce });
+  }
 
-
-  useEffect(() => {
-    showButton();
-    window.addEventListener("resize", showButton);
-    return {
-      // window.removeEventListener('resize', showButton)
-    };
-  }, []);
-
-  // Getting Balance of MetaMask Account
-  useEffect(() => {
-    async function getBalance() {
-      const web3 = window.web3;
-      const webeProvider = new Web3(
-        Web3.givenProvider || "http://localhost:7545"
-      );
-      const accounts = await webeProvider.eth.getAccounts();
-
-      const blnce = web3.utils.fromWei(
-        await web3.eth.getBalance(accounts[0]),
-        "ether"
-      );
-      console.log(blnce);
-      setbalance(blnce);
-    }
-    getBalance();
-
-    setName(cookies.get("Username"));
-
-  }, []);
-
-  
 
   async logout() {
     const web3 = window.web3;
@@ -126,221 +136,226 @@ function Navbar() {
     window.location = "/";
   }
 
+  // if (cookies.get("checkIsUser")) {
+  //   console.log("checkIsUser: ", cookies.get("checkIsUser"));
+
+  //   return <Home />;
+  // } else {
+  //   console.log("checkIsUser: ", cookies.get("checkIsUser"));
+  //   return <LoginSignUp />;
+  // }
 
 
-  if (cookies.get("checkIsUser")) {
-    console.log("checkIsUser: ", cookies.get("checkIsUser"));
-
-    return <Home />;
-  } else {
-    console.log("checkIsUser: ", cookies.get("checkIsUser"));
-    return <LoginSignUp />;
-  }
-
-  function Home(props) {
+  render() {
     return (
       <div>
-        <IconContext.Provider value={{ color: "#fff" }}>
-          <nav className="navbar">
-            <div className="navbar-container container">
-              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                {/* <MdFingerprint className="navbar-icon" /> */}
-                <img
-                  src={logo}
-                  className="App-logo"
-                  alt="Logo"
-                  width="40"
-                  height="40"
-                />
-                &nbsp;&nbsp;{" "}
-                <p>
-                  <font color="#EF8E19">Blockchain</font> Land Records System
-                </p>
-              </Link>
-              <div className="menu-icon" onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
-              </div>
-              <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                  <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                    {" "}
-                    Home
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link
-                    to="/search-property"
-                    className="nav-links"
-                    onClick={closeMobileMenu}
-                  >
-                    Explore
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link
-                    to="/properties"
-                    className="nav-links"
-                    onClick={closeMobileMenu}
-                  >
-                    Properties
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/requests"
-                    className="nav-links"
-                    onClick={closeMobileMenu}
-                  >
-                    Requests
-                  </Link>
-                </li>
-
-                <div className="menu-container">
-                  <button onClick={onClick} className="menu-trigger">
-                    <span>User</span>
-                    <img
-                      width="40"
-                      height="40"
-                      src={profileLink}
-                      alt="User avatar"
-                    />
-                  </button>
-                  <nav
-                    ref={dropdownRef}
-                    className={`menu ${isActive ? "active" : "inactive"}`}
-                  >
-                    <ul>
-                      <li>
-                        <h3 style={{ color: "#35A246" }}>{name}</h3>
-                      </li>
-                      <li>
-                        <h4 style={{ color: "#EF8E19" }}>
-                          Balance: {balance} ETH
-                        </h4>
-                      </li>
-                      <li>
-                        <Link
-                          to="/profile"
-                          className="nav-links"
-                          onClick={closeMobileMenu}
-                        >
-                          Profile
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/requested-lands"
-                          className="nav-links"
-                          onClick={closeMobileMenu}
-                        >
-                          Requested Lands
-                        </Link>
-                      </li>
-                      <li>
-                        {button ? (
-                          <Link to="/">
-                            <Button
-                              buttonSize="btn--wide"
-                            >
-                              Sign Out
-                            </Button>
-                          </Link>
-                        ) : (
-                          <Link to="/">
-                            <Button
-                              buttonSize="btn--wide"
-                            >
-                              Sign Out
-                            </Button>
-                          </Link>
-                        )}
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </ul>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <nav className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={this.state.closeMobileMenu}>
+              {/* <MdFingerprint className="navbar-icon" /> */}
+              <img
+                src={logo}
+                className="App-logo"
+                alt="Logo"
+                width="40"
+                height="40"
+              />
+              &nbsp;&nbsp;{" "}
+              <p>
+                <font color="#EF8E19">Blockchain</font> Land Records System
+              </p>
+            </Link>
+            <div className="menu-icon" onClick={this.state.handleClick}>
+              {this.state.click ? <FaTimes /> : <FaBars />}
             </div>
-          </nav>
-        </IconContext.Provider>
-      </div>
-    );
+            <ul className={this.state.click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={this.state.closeMobileMenu}>
+                  {" "}
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="/search-property"
+                  className="nav-links"
+                  onClick={this.state.closeMobileMenu}
+                >
+                  Explore
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="/properties"
+                  className="nav-links"
+                  onClick={this.state.closeMobileMenu}
+                >
+                  Properties
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/requests"
+                  className="nav-links"
+                  onClick={this.state.closeMobileMenu}
+                >
+                  Requests
+                </Link>
+              </li>
+
+              <div className="menu-container">
+                <button onClick={this.state.onClick} className="menu-trigger">
+                  <span>User</span>
+                  <img
+                    width="40"
+                    height="40"
+                    src={this.state.profileLink}
+                    alt="User avatar"
+                  />
+                </button>
+                <nav
+                  ref={this.state.dropdownRef}
+                  className={`menu ${this.state.isActive ? "active" : "inactive"}`}
+                >
+                  <ul>
+                    <li>
+                      <h3 style={{ color: "#35A246" }}>{this.state.name}</h3>
+                    </li>
+                    <li>
+                      <h4 style={{ color: "#EF8E19" }}>
+                        Balance: {this.state.balance} ETH
+                      </h4>
+                    </li>
+                    <li>
+                      <Link
+                        to="/profile"
+                        className="nav-links"
+                        onClick={this.state.closeMobileMenu}
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/requested-lands"
+                        className="nav-links"
+                        onClick={this.state.closeMobileMenu}
+                      >
+                        Requested Lands
+                      </Link>
+                    </li>
+                    <li>
+                      {button ? (
+                        <Link to="/">
+                          <Button
+                            buttonSize="btn--wide"
+                          >
+                            Sign Out
+                          </Button>
+                        </Link>
+                      ) : (
+                        <Link to="/">
+                          <Button
+                            buttonSize="btn--wide"
+                          >
+                            Sign Out
+                          </Button>
+                        </Link>
+                      )}
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
+    </div>
+    )
   }
 
-  function LoginSignUp(props) {
-    return (
-      <div>
-        <IconContext.Provider value={{ color: "#fff" }}>
-          <nav className="navbar">
-            <div className="navbar-container container">
-              <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                {/* <MdFingerprint className="navbar-icon" />
-                Blockchain Land Records System */}
-                {/* <img src={logo} alt="Logo" width="40" height="40" />&nbsp;<p style={{ color: '#EF8E19' }}>Blockchain</p> &nbsp; <p>Land Records System</p> */}
-                <img
-                  src={logo}
-                  className="App-logo"
-                  alt="Logo"
-                  width="40"
-                  height="40"
-                />{" "}
-                &nbsp;&nbsp;{" "}
-                <p>
-                  <font color="#EF8E19">Blockchain</font> Land Records System
-                </p>
-              </Link>
-              <div className="menu-icon" onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
-              </div>
-              <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                  <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                    Home
-                  </Link>
-                </li>
+  // function Home(props) {
+  //   return (
+      
+  //   );
+  // }
 
-                <li className="nav-btn">
-                  {button ? (
-                    <Link to="/sign-in" className="btn-link">
-                      <Button buttonStyle="btn--outline">SIGN IN</Button>
-                    </Link>
-                  ) : (
-                    <Link to="/sign-in" className="btn-link">
-                      <Button
-                        buttonStyle="btn--outline"
-                        buttonSize="btn--mobile"
-                        onClick={closeMobileMenu}
-                      >
-                        SIGN IN
-                      </Button>
-                    </Link>
-                  )}
-                </li>
-                <li className="nav-btn">
-                  {button ? (
-                    <Link to="/sign-up" className="btn-link">
-                      <Button buttonStyle="btn--outline">SIGN UP</Button>
-                    </Link>
-                  ) : (
-                    <Link to="/sign-up" className="btn-link">
-                      <Button
-                        buttonStyle="btn--outline"
-                        buttonSize="btn--mobile"
-                        onClick={closeMobileMenu}
-                      >
-                        SIGN UP
-                      </Button>
-                    </Link>
-                  )}
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </IconContext.Provider>
-      </div>
-    );
-  }
+  // function LoginSignUp(props) {
+  //   return (
+  //     <div>
+  //       <IconContext.Provider value={{ color: "#fff" }}>
+  //         <nav className="navbar">
+  //           <div className="navbar-container container">
+  //             <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+  //               {/* <MdFingerprint className="navbar-icon" />
+  //               Blockchain Land Records System */}
+  //               {/* <img src={logo} alt="Logo" width="40" height="40" />&nbsp;<p style={{ color: '#EF8E19' }}>Blockchain</p> &nbsp; <p>Land Records System</p> */}
+  //               <img
+  //                 src={logo}
+  //                 className="App-logo"
+  //                 alt="Logo"
+  //                 width="40"
+  //                 height="40"
+  //               />{" "}
+  //               &nbsp;&nbsp;{" "}
+  //               <p>
+  //                 <font color="#EF8E19">Blockchain</font> Land Records System
+  //               </p>
+  //             </Link>
+  //             <div className="menu-icon" onClick={handleClick}>
+  //               {click ? <FaTimes /> : <FaBars />}
+  //             </div>
+  //             <ul className={click ? "nav-menu active" : "nav-menu"}>
+  //               <li className="nav-item">
+  //                 <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+  //                   Home
+  //                 </Link>
+  //               </li>
+
+  //               <li className="nav-btn">
+  //                 {button ? (
+  //                   <Link to="/sign-in" className="btn-link">
+  //                     <Button buttonStyle="btn--outline">SIGN IN</Button>
+  //                   </Link>
+  //                 ) : (
+  //                   <Link to="/sign-in" className="btn-link">
+  //                     <Button
+  //                       buttonStyle="btn--outline"
+  //                       buttonSize="btn--mobile"
+  //                       onClick={closeMobileMenu}
+  //                     >
+  //                       SIGN IN
+  //                     </Button>
+  //                   </Link>
+  //                 )}
+  //               </li>
+  //               <li className="nav-btn">
+  //                 {button ? (
+  //                   <Link to="/sign-up" className="btn-link">
+  //                     <Button buttonStyle="btn--outline">SIGN UP</Button>
+  //                   </Link>
+  //                 ) : (
+  //                   <Link to="/sign-up" className="btn-link">
+  //                     <Button
+  //                       buttonStyle="btn--outline"
+  //                       buttonSize="btn--mobile"
+  //                       onClick={closeMobileMenu}
+  //                     >
+  //                       SIGN UP
+  //                     </Button>
+  //                   </Link>
+  //                 )}
+  //               </li>
+  //             </ul>
+  //           </div>
+  //         </nav>
+  //       </IconContext.Provider>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Navbar;
