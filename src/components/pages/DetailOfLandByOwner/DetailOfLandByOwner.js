83122d0086c3aeeed80a4f23e5daa4cb1763bfe4
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-17 17:58:26 +0500

    btn position change

--- src/components/pages/DetailOfLandByOwner/DetailOfLandByOwner.js
+++ src/components/pages/DetailOfLandByOwner/DetailOfLandByOwner.js
@@ -547,12 +547,6 @@ class DetailOfLandByOwner extends Component {
               }}
             >
               <div className="home__hero-text-wrapper">
-                <div className="top-line">
-                  {"View Registered Land by Owner"}
-                </div>
-                <h1 className={true ? "heading" : "heading dark"}>
-                  {"Registered Land Detail"}
-                </h1>
                 <Link to={"/properties"}>
                   <Button
                     buttonStyle="btn--primary"
@@ -562,6 +556,10 @@ class DetailOfLandByOwner extends Component {
                     &#x2B05;{" Back"}
                   </Button>
                 </Link>
+                <div className="top-line">{"View Land by Owner"}</div>
+                <h1 className={true ? "heading" : "heading dark"}>
+                  {"Land No: "} {this.state.Id}
+                </h1>
               </div>
             </div>
 
